e8bf6e75d11a8412fbf1df5b263d9dec6c9f81fb
wuxinxinya <1252857980@qq.com>
2020-04-23 11:39:02 +0800

    首页-租房小组-标题布局

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -4,39 +4,9 @@ import { Carousel, Flex } from 'antd-mobile';
 // import axios from 'axios'
 import { BASE_URL } from '../../utils/axios'
 import { getSwiper } from '../../utils/api/Home';
-// 导入的是图片的64位格式
-import Nav1 from '../../assets/images/nav-1.png'
-import Nav2 from '../../assets/images/nav-2.png'
-import Nav3 from '../../assets/images/nav-3.png'
-import Nav4 from '../../assets/images/nav-4.png'
-import './index.css'
-// 首页栏目导航的数据
-const navs = [
-    {
-        id: 1,
-        img: Nav1,
-        title: '整租',
-        path: '/home/house'
-    },
-    {
-        id: 2,
-        img: Nav2,
-        title: '合租',
-        path: '/home/house'
-    },
-    {
-        id: 3,
-        img: Nav3,
-        title: '地图找房',
-        path: '/map'
-    },
-    {
-        id: 4,
-        img: Nav4,
-        title: '去出租',
-        path: '/rent/add'
-    }
-]
+import './index.scss'
+import navs from '../../utils/navconfig';
+
 class Index extends Component {
     state = {
         // 轮播图数据
@@ -68,47 +38,70 @@ class Index extends Component {
             })
         }
     }
+    // 渲染轮播图{/* autoplay={true}轮播图自动播放 infinite无限循环*/}
+    renderSwiper = () => {
+        return (
+            <Carousel
+                autoplay={this.state.isPlay}
+                infinite
+                autoplayInterval={2000}
+            >
+                {this.state.swiper.map(val => (
+                    <a
+                        key={val.id}
+                        href="http://www.itheima.com"
+                        style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                    >
+                        <img
+                            src={`${BASE_URL}${val.imgSrc}`}
+                            alt=""
+                            style={{ width: '100%', verticalAlign: 'top' }}
+                            onLoad={() => {
+                                // fire window resize event to change height
+                                // 窗口大小改变时候的自适应----适配问题
+                                window.dispatchEvent(new Event('resize'));
+                                this.setState({ imgHeight: 'auto' });
+                            }}
+                        />
+                    </a>
+                ))}
+            </Carousel>)
+    }
+    // 栏目导航
+    renderNavs = () => {
+        return (
+            <Flex className='nav'>
+                {navs.map((item) =>
+                    <Flex.Item key={item.id} onClick={() => {
+                        this.props.history.push(item.path)
+                    }}>
+                        <img src={item.img} alt='' />
+                        <p>{item.title}</p>
+                    </Flex.Item>
+                )}
+
+            </Flex>)
+    }
+    // 租房小组
+    renderHouse = () => {
+        return (
+            <div className="group">
+                <Flex className="group-title" justify="between">
+                    <h3>租房小组</h3>
+                    <span>更多</span>
+                </Flex>
+            </div>
+        )
+    }
     render() {
         return (
             <div className='index'>
-                {/* autoplay={true}轮播图自动播放 infinite无限循环*/}
-                <Carousel
-                    autoplay={this.state.isPlay}
-                    infinite
-                    autoplayInterval={2000}
-                >
-                    {this.state.swiper.map(val => (
-                        <a
-                            key={val.id}
-                            href="http://www.itheima.com"
-                            style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
-                        >
-                            <img
-                                src={`${BASE_URL}${val.imgSrc}`}
-                                alt=""
-                                style={{ width: '100%', verticalAlign: 'top' }}
-                                onLoad={() => {
-                                    // fire window resize event to change height
-                                    // 窗口大小改变时候的自适应----适配问题
-                                    window.dispatchEvent(new Event('resize'));
-                                    this.setState({ imgHeight: 'auto' });
-                                }}
-                            />
-                        </a>
-                    ))}
-                </Carousel>
+                {/* 轮播图 */}
+                {this.renderSwiper()}
                 {/* 栏目布局 */}
-                <Flex className='nav'>
-                    {navs.map((item) =>
-                        <Flex.Item key={item.id} onClick={() => {
-                            this.props.history.push(item.path)
-                        }}>
-                            <img src={item.img} />
-                            <p>{item.title}</p>
-                        </Flex.Item>
-                    )}
-
-                </Flex>
+                {this.renderNavs()}
+                {/* 租房小组 */}
+                {this.renderHouse()}
             </div>
         );
     }
